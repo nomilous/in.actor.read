754adcdeddfa50d27f348e1a552eba5e15a24856
nomilous <richard@clue.co.za>
2015-07-06 18:26:22 +0200

    read

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # in.actor.read
+
 File reader for in.
+
+eg.
+
+```javascript
+$$in(function(readme) { // in. read README.md
+  
+})
+```
+
+```javascript
+$$in(function(lines) { // in.as.lines read ./README.md
+  
+})
+```
+
+```javascript
+$$in(function(stream) { // in.as.stream read {{process.cwd()}}/README.md
+  stream.on('data', ...
+  stream.on('end', ...
+  stream.on('error', ...
+})
+```
+
+```javascript
+$$in(function(streams) { // in.as.stream.lines read {{ $$files('~/repos/**/README.md') }}
+  streams.forEach(function(stream) {
+    // one stream per found readme
+    stream.on('data', ... // for each line
+    stream.on('end', ...
+    stream.on('error', ...
+})
+```
+
+```javascript
+$$in(function(raw) { // in.as.buffer read CANT_README
+  
+})
+```
+
+```javascript
+$$in(function(raw) { // in.as.stream.buffer read /dev/random
+  stream.on('data', ...
+  stream.on('end', ...
+  stream.on('error', ...
+})
+```
+
+NB __It accepts only one filename!__
+
+```javascript
+$$in(function(lines) { // in.as.lines read file1.txt file2.txt
+    
+  // will attempt to read 'file1.txt file2.txt'
+})
+```
---
+++ index.js
@@ -0,0 +1,61 @@
+var fs = require('fs');
+var path = require('path');
+var EventEmitter = require('events').EventEmitter;
+
+$$in.actors.read = function(opts, inArgs, actionArg, actorPath) {
+
+  var readStream, emitter, options;
+  var isStream = actionArg.adapters.indexOf('stream') >= 0;
+  var isBuffer = actionArg.adapters.indexOf('buffer') >= 0;
+  var file = actionArg.expansion.trim();
+
+  if (path.parse(file).root == '') {
+    file = path.dirname(opts.$$caller.FileName) + path.sep + file;
+  }
+
+  options = util._extend({}, opts);
+  options.encoding = Buffer.isEncoding(options.encoding) ? options.encoding : 'utf8';
+  options.flag = (options.flag || 'r');
+
+  if (isStream || isBuffer) {
+    readStream = fs.createReadStream(file);
+    if (isStream && isBuffer) {
+      actionArg.value = readStream;
+      return
+    }
+
+    if (isStream) {
+      actionArg.value = emitter = new EventEmitter();
+
+      readStream.on('error', function(e) {
+        emitter.emit('error', e);
+      });
+
+      readStream.on('end', function() {
+        emitter.emit('end');
+      });
+
+      readStream.on('data', function(chunk) {
+        if (isBuffer) return emitter.emit(data, chunk);
+        emitter.emit('data', chunk.toString(options.encoding || 'utf8'));
+      });
+
+      return;
+    }
+  }
+
+  if (isBuffer) delete options.encoding;
+
+  return $$in.promise(function(resolve, reject) {
+
+    fs.readFile(file, options, function(err, data) {
+      if (err) return reject(err);
+      actionArg.value = data;
+      resolve(actionArg);
+    })
+  });
+}
+
+$$in.actors.read.$$can = function(action) {
+  return true;
+}
